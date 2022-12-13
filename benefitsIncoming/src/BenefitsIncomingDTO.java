c87cfca32a372b53a370098c272b308f50f78f8b
Evan <evan@ivanhoe.attlocal.net>
2022-12-12 22:42:08 -0600

    benifitsIncoming setup

--- benefitsIncoming/src/BenefitsIncomingDTO.java
+++ benefitsIncoming/src/BenefitsIncomingDTO.java
@@ -1,3 +1,48 @@
 public class BenefitsIncomingDTO {
+  private String userId;
+  private String firstName;
+  private String lastName;
+  private Integer version;
+  private String InsuranceCompany;
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Integer getVersion() {
+    return version;
+  }
+
+  public void setVersion(Integer version) {
+    this.version = version;
+  }
+
+  public String getInsuranceCompany() {
+    return InsuranceCompany;
+  }
+
+  public void setInsuranceCompany(String insuranceCompany) {
+    InsuranceCompany = insuranceCompany;
+  }
 
 }
--- benefitsIncoming/src/BenefitsIncomingSession.java
+++ benefitsIncoming/src/BenefitsIncomingSession.java
@@ -1,7 +1,9 @@
 public class BenefitsIncomingSession {
 
   public static void main(String[] args) {
-
+    // For the files in CSV format
+    // Read the content of the file and separate enrollees by insurance company in its own file.
+    // Additionally, sort the contents of each file by last and first name (ascending).
   }
 
 }
